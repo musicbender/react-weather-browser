1ac25af31f5b8ee22dd3ae8c428f656ebf1d7354
Pat Jacobs <musicbender@gmail.com>
2016-11-11 08:43:51 -0800

    added charts with sparkline and average numbers from data

--- src/containers/weather-list.jsx
+++ src/containers/weather-list.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart.jsx';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
         
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="orange" /></td>
+                <td><Chart data={pressure} color="green" /></td>
+                <td><Chart data={humidity} color="blue" /></td>
             </tr>
-        )
+        );
     }
     
     render() {
